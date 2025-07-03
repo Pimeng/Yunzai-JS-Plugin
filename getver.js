825df59e278bc05b79de50610ea123e290f12440
皮梦 <pimeng@pimeng.icu>
2025-07-03 02:48:03 +0000

    fix: 野生云崽
    Signed-off-by: 皮梦 <pimeng@pimeng.icu>

--- getver.js
+++ getver.js
@@ -1,35 +1,26 @@
 import plugin from '../../lib/plugins/plugin.js';
 import fs from 'node:fs'
 
-let packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+let p = JSON.parse(fs.readFileSync('package.json', 'utf8'))
 
-export class GetVer extends plugin {
+export class GetVersion extends plugin {
     constructor() {
       super({
         name: 'GetVersion',
-        dsc: '简单获取当前运行的云崽分支',
+        dsc: '简单获取当前运行的云崽版本',
         event: 'message',
         priority: 100,
         rule: [
           {
-            reg: '^/getver$', // 命令
+            reg: '^/getver',
             fnc: 'getyunver'
           }
         ]
       });
     }
-  
-    async getyunver(e){
-        let name = 'Yunzai-Bot'
-        if (packageJson.name === 'miao-yunzai') {
-            name = 'Miao-Yunzai'
-          } else if (packageJson.name === 'trss-yunzai') {
-            name = 'TRSS-Yunzai'
-          } 
-          else if (packageJson.name === 'a-yunzai') {
-            name = 'A-Yunzai'
-          }
-        e.reply(`当前云崽版本：${name}`)
-    }
 
+    async getyunver(e){
+        const n = p.name.replace(/(^\w|-\w)/g, s => s.toUpperCase())
+        e.reply(`当前云崽分支：${n}`)
+    }
 }
